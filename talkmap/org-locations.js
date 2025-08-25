eaaa30ab05cf8daf07531f2e76abaef36e1124b0
cqewxyj1ella <122031492@qq.com>
2025-08-25 04:17:57 +0000

    updated and enabled talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,12 @@
 var addressPoints = [
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.4893335,
-    -0.14405508452768728
+    "Theory Student Day 2025<br />University of Science and Technology of China, Theory Group; Hefei, Anhui, China",
+    31.8665676,
+    117.281428
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
+    "Women in TCS 2025 Rump Session<br />UC-Berkeley Simnon's Institute; Berkeley, CA, USA",
     37.8708393,
     -122.272863
-  ],
-  [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
-  ],
-  [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
   ]
 ];
